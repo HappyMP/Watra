5464c8e1775bd71b281d3c68b9f272a47afd4954
Mateusz <mateuszpek00@gmail.com>
2026-05-03 21:19:12 +0200

    feat(phase-5): extend Product with city, defaultVenue, eventStatus, eventType, isOnline

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use App\Entity\Catalog\City;
+use App\Entity\Catalog\Venue;
+use App\Enum\EventStatus;
+use App\Enum\EventType;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
@@ -15,6 +19,73 @@ use Sylius\MolliePlugin\Entity\ProductTrait;
 class Product extends BaseProduct implements ProductInterface
 {
     use ProductTrait;
+
+    #[ORM\ManyToOne(targetEntity: City::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?City $city = null;
+
+    #[ORM\ManyToOne(targetEntity: Venue::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?Venue $defaultVenue = null;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => false])]
+    private bool $isOnline = false;
+
+    #[ORM\Column(type: 'string', length: 32, enumType: EventStatus::class, options: ['default' => 'draft'])]
+    private EventStatus $eventStatus = EventStatus::DRAFT;
+
+    #[ORM\Column(type: 'string', length: 32, enumType: EventType::class, options: ['default' => 'other'])]
+    private EventType $eventType = EventType::OTHER;
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getDefaultVenue(): ?Venue
+    {
+        return $this->defaultVenue;
+    }
+
+    public function setDefaultVenue(?Venue $defaultVenue): void
+    {
+        $this->defaultVenue = $defaultVenue;
+    }
+
+    public function isOnline(): bool
+    {
+        return $this->isOnline;
+    }
+
+    public function setIsOnline(bool $isOnline): void
+    {
+        $this->isOnline = $isOnline;
+    }
+
+    public function getEventStatus(): EventStatus
+    {
+        return $this->eventStatus;
+    }
+
+    public function setEventStatus(EventStatus $eventStatus): void
+    {
+        $this->eventStatus = $eventStatus;
+    }
+
+    public function getEventType(): EventType
+    {
+        return $this->eventType;
+    }
+
+    public function setEventType(EventType $eventType): void
+    {
+        $this->eventType = $eventType;
+    }
 
     protected function createTranslation(): ProductTranslationInterface
     {
