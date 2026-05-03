661816124a6243381ce403dec0062931acbdbe0a
Mateusz <mateuszpek00@gmail.com>
2026-05-03 21:19:59 +0200

    feat(phase-5): extend AdminUser with M2M administrationRoles collection

--- src/Entity/User/AdminUser.php
+++ src/Entity/User/AdminUser.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Entity\User;
 
+use App\Entity\Admin\AdministrationRole;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\AdminUser as BaseAdminUser;
 use Sylius\MolliePlugin\Entity\OnboardingStatusAwareInterface;
@@ -14,4 +17,33 @@ use Sylius\MolliePlugin\Entity\OnboardingStatusAwareTrait;
 class AdminUser extends BaseAdminUser implements OnboardingStatusAwareInterface
 {
     use OnboardingStatusAwareTrait;
+
+    /** @var Collection<int, AdministrationRole> */
+    #[ORM\ManyToMany(targetEntity: AdministrationRole::class)]
+    #[ORM\JoinTable(name: 'watra_admin_user_administration_role')]
+    private Collection $administrationRoles;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->administrationRoles = new ArrayCollection();
+    }
+
+    /** @return Collection<int, AdministrationRole> */
+    public function getAdministrationRoles(): Collection
+    {
+        return $this->administrationRoles;
+    }
+
+    public function addAdministrationRole(AdministrationRole $role): void
+    {
+        if (!$this->administrationRoles->contains($role)) {
+            $this->administrationRoles->add($role);
+        }
+    }
+
+    public function removeAdministrationRole(AdministrationRole $role): void
+    {
+        $this->administrationRoles->removeElement($role);
+    }
 }
